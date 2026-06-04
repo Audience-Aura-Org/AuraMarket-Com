a828fa64190382c3cf4938a45c226718811f291b
zeroxerxes <99093016+zeroxerxes@users.noreply.github.com>
2026-06-04 11:21:19 +0100

    Fix Android keyboard resizing in chat

--- web/android/app/src/main/java/com/auramarket/app/MainActivity.java
+++ web/android/app/src/main/java/com/auramarket/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.auradime.app;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        super.onCreate(savedInstanceState);
+    }
+}
