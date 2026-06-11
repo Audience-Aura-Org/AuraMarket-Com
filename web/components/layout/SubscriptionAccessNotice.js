ac5a27fbc1c8ee5b5bfd07073f9f42fd3c4716af
zeroxerxes <99093016+zeroxerxes@users.noreply.github.com>
2026-06-11 21:51:08 +0100

    Hide full subscription notice

--- web/components/layout/SubscriptionAccessNotice.js
+++ web/components/layout/SubscriptionAccessNotice.js
@@ -123,7 +123,8 @@ export default function SubscriptionAccessNotice({ disabled = false }) {
     if (hideKey) {
       window.localStorage.setItem(hideKey, String(Date.now() + HIDE_MS));
     }
-    setHidden(true);
+    setNotice(null);
+    setHidden(false);
   };
 
   return (
